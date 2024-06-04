6b771b2e9f6697ad49f770d9f3d3311f4dd6af03
Tim Foard Jr <timfoardjr@outlook.com>
2024-06-03 22:04:32 -0400

    Add Node component and use it in the Linked List page

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
           <div className="w-full flex-none md:w-64">
             <SideNav />
           </div>
--- app/linked-lists/page.tsx
+++ app/linked-lists/page.tsx
@@ -1,8 +1,19 @@
+import Node from '@/app/ui/node';
+
 export default function Page() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h3>Linked Lists</h3>
-      </main>
+      <div className="">
+        <h1 className="text-4xl text-center">Linked Lists</h1>
+        <div className="flex space-x-10">
+          <Node node={{name: "1"}}/>
+          <Node node={{name: "2"}}/>
+          <Node node={{name: "3"}}/>
+          <Node node={{name: "3"}}/>
+          <Node node={{name: "3"}}/>
+          <Node node={{name: "3"}}/>
+          <Node node={{name: "3"}}/>
+        </div>
+      </div>
     );
   }
   
